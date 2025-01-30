57241b5bd99db513d0122b0364e90930407f5602
songsapk <songsap98938@gmail.com>
2025-01-30 20:29:22 +0700

    Tnitial express setup

---
+++ week4/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({ success: true, data: { id: 1 } });
+});
+
+// Define a port (default to 5000 if not in .env)
+const PORT = process.env.PORT || 5000;
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
